1e19d251696a561e1116f51461ddb97e97e0d354
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-26 15:16:59 +0300

    create a request listener function explicitly then pass it as an argument to the createServer() function

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const http = require('http');
 
+//Request listener function which will be an argument in the 
+//createServer() function
+function rqListener(req, res){
+
+}
+
+http.createServer(rqListener);

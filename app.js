5948451afa0d91c18253ba5646776249f8f86c53
Collins Kariuki <collinsmunkary@gmail.com>
2022-07-27 08:39:22 +0300

    sending a response from the server

--- app.js
+++ app.js
@@ -16,8 +16,19 @@ const http = require('http');
 //createServer returns a server which you shoudld save in a constant
 //or variable, const preferred
 const server = http.createServer((req,res) => {
-    console.log(req.url, req.method, req.headers);
+    // console.log(req.url, req.method, req.headers);
     // process.exit();
+
+    //set headers in the response
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+    res.write('</html>');
+
+    //tell node that we are done writing the response
+    //no other code should come after this
+    res.end();
 
 })
 
